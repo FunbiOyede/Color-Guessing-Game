59f978957eb5530313409bf9552c0679b317ece1
Funbi oyede <samsonoyede@yahoo.com>
2019-04-11 09:54:43 +0100

    first commit

---
+++ Appilcation.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Color_Guessing_Game
+{
+    class AppLogic
+    {
+
+        public static void ProcessUsername(ConsoleColor color)
+        {
+            string username;
+
+            Console.ForegroundColor = color;
+
+            Console.WriteLine("Please enter your name ?");
+
+            username = Console.ReadLine().ToUpper();
+            Console.ResetColor();
+
+            Console.WriteLine("Player:  {0}", username.ToUpper());
+       
+        }
+
+        public static void DisplayQuestion(ConsoleColor color)
+        {
+            Console.WriteLine("This is how the game would work, I would display a list of colors and i would choose one color from the list, your job is to guess the color i choose.");
+          
+                string[] colors = { "Red", "Yellow", "Blue", "Purple", "Orange", "Black" };
+
+                NEWGAME:
+                Console.WriteLine("These are the colors");
+
+                //Display List of color int tje colors array
+                for (int i = 0; i < colors.Length; i++)
+                {
+                    Console.WriteLine(colors[i]);
+                }
+
+
+                Random random = new Random();
+
+                int colorIndex = random.Next(colors.Length);
+
+                string chosenColor = colors[colorIndex];
+
+                Console.ForegroundColor = color;
+
+                Console.WriteLine("  Guess The color i choose ?");
+
+                GuessAgain:
+                string answer = Console.ReadLine();
+                if (answer != chosenColor)
+                {
+                    PrintWrongMessage(ConsoleColor.Red, "Wrong guess , Guess again");
+                    goto GuessAgain;
+
+                }
+                else if (answer == chosenColor)
+                {
+                    PrintSuccessMessage(ConsoleColor.Green, "Correct");
+                }
+
+
+                Console.ResetColor();
+
+                Console.WriteLine("Play Again ? [Y or N]");
+                string PlayAgain = Console.ReadLine().ToUpper();
+                if (PlayAgain == "Y")
+                {
+                    goto NEWGAME;
+                }
+                else if (PlayAgain == "N")
+                {
+                    return;
+                }
+            
+  
+        }
+
+           
+       public static void PrintWrongMessage(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+  
+        public static void PrintSuccessMessage(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Color_Guessing_Game
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            string AppName = "Color guessing game";
+            string AppVersion = "1.0.0";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //diplaying application version
+            Console.WriteLine("{0} : version {1} ", AppName, AppVersion);
+            Console.ResetColor();
+            Console.WriteLine("Welcome to the game of guessing colors");
+
+            //getting username
+            AppLogic.ProcessUsername(ConsoleColor.DarkMagenta);
+
+            //Displaying the colors 
+            AppLogic.DisplayQuestion(ConsoleColor.Magenta);
+        }
+
+    }
+}
